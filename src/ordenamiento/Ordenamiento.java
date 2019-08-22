dd5b0863f0c15ab828c1320b451846d1ff18e698
alumno <alumno@LAB2C1.labvn.local>
2019-08-22 08:43:01 -0600

    Examen

---
+++ src/ordenamiento/Ordenamiento.java
@@ -0,0 +1,35 @@
+package ordenamiento;
+import java.util.Scanner;
+public class Ordenamiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Scanner var=new Scanner(System.in);
+        int NO1,NO2,NO4,NO3,AUX1;
+        String CA1,CA2,CA3,CA4;
+        
+        System.out.println("Ingrese Numero de Carnet 1: ");
+        CA1=var.nextLine();
+        System.out.println("Ingrese Nota 1: ");
+        NO1=var.nextInt();
+        System.out.println("Ingrese Numero de Carnet 2: ");
+        CA2=var.nextLine();
+        System.out.println("Ingrese Nota 2: ");
+        NO2=var.nextInt();
+        System.out.println("Ingrese Numero de Carnet 3: ");
+        CA3=var.nextLine();
+        System.out.println("Ingrese Nota 3: ");
+        NO3=var.nextInt();
+        System.out.println("Ingrese Numero de Carnet 4: ");
+        CA4=var.nextLine();
+        System.out.println("Ingrese Nota 4: ");
+        NO4=var.nextInt();
+        
+        
+        
+    }
+    
+}
